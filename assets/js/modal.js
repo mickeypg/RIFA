044c62b5e52495a7addef7532091f800e6683841
mpincay <miguel.pincay@gmail.com>
2018-12-01 13:13:59 -0500

    updated javascript

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -2,14 +2,21 @@
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
+
+for (i=0; i <= 7; i++ ){
+  let currentImgId = 'myImg' + i;
+  var img = document.getElementById(currentImgId);
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
+  img.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
+  }
 }
+
+
+
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
